6987787a51718c19392d651eacdaab7f91bb3fb0
JoseDG <josethp1@gmail.com>
2023-12-09 13:30:04 -0500

    ejemplo interfaces

---
+++ Interfaces/AvisosVarios/AvisosVarios/IAvisos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisosVarios
+{
+    interface IAvisos
+    {
+        void mostrarAviso();
+
+        string getFecha();
+    }
+}
---
+++ Interfaces/AvisosVarios/AvisosVarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AvisosVarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AvisosTrafico av1 = new AvisosTrafico();
+            av1.mostrarAviso();
+
+            AvisosTrafico av2 = new AvisosTrafico("PJ LOJA", "Sancion por escandolo publico: $500", "02-05-2023");
+            av2.getFecha();
+            av2.mostrarAviso();
+        }
+    }
+}
