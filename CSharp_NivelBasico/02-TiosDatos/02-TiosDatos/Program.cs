5963650452329323948aeba5709ffcbe9f443459
JoseDG <josethp1@gmail.com>
2023-12-11 21:57:30 -0500

    tios de datos

---
+++ CSharp_NivelBasico/02-TiosDatos/02-TiosDatos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_TiosDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion de variables
+            int edad;
+            string nombre = "Jose";
+            edad = 34;
+            double sueldo = 1025.36;
+            bool casado = false;
+
+            //con el signo + es para concatenar
+            Console.WriteLine("Mi nombre es: " + nombre);
+            Console.WriteLine("Mi edad es: " + edad);
+            Console.WriteLine("Mi sueldo es de: " + sueldo);
+            Console.WriteLine("Estas casado?: " + casado);
+            Console.ReadKey();
+        }
+    }
+}
