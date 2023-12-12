c9d1ea357cf927a49dfa80842f28c915444ef3d9
JoseDG <josethp1@gmail.com>
2023-12-11 21:30:23 -0500

    hola mundo

---
+++ CSharp_NivelBasico/SolucionNivelBasico/01-HolaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.ReadKey();
+        }
+    }
+}
