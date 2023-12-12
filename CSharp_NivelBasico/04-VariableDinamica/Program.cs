e0b824b9ed0ed82af2d181190fc7bd3e21444768
JoseDG <josethp1@gmail.com>
2023-12-11 22:17:58 -0500

    variable dynamic

---
+++ CSharp_NivelBasico/04-VariableDinamica/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_VariableDinamica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic Contenido;
+            Contenido = "Bienvenido a mi Sistema";
+
+            Console.WriteLine("Contenido: " + Contenido);
+        }
+    }
+}
