31a5116496eff545bcc1db96209c9b12413b3680
JoseDG <josethp1@gmail.com>
2023-12-13 09:52:26 -0500

    add metodos 01

---
+++ CSharp_NivelBasico/16-MetodoEjemplo1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_MetodoEjemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void IngresoDatos()
+        {
+
+        }
+    }
+}
