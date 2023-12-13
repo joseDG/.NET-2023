0e13622a9fd4da1e378fa830118289209e66ebc8
JoseDG <josethp1@gmail.com>
2023-12-13 09:32:34 -0500

    add cicloWhile

---
+++ CSharp_NivelBasico/11-EstructuraCiclicaDoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_EstructuraCiclicaDoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero;
+            Numero = 0;
+
+            do
+            {
+                Console.WriteLine("El valor de la variable Numero es: " + Numero);
+                Numero++;
+
+            } while (Numero <= 10);
+            Console.ReadKey();
+        }
+    }
+}
