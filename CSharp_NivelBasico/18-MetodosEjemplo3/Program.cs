1936ad824d29d74018e8372fbcce49deb7011684
JoseDG <josethp1@gmail.com>
2023-12-13 10:52:34 -0500

    update metodo 3

--- CSharp_NivelBasico/18-MetodosEjemplo3/Program.cs
+++ CSharp_NivelBasico/18-MetodosEjemplo3/Program.cs
@@ -10,11 +10,30 @@ namespace _18_MetodosEjemplo3
     {
         static void Main(string[] args)
         {
+            string Nombre;
+            double PagoxDia;
+            int Dias;
+
+            Console.Write("Ingresa tu Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.Write("Cuanto te pagan por día trabajado?: ");
+            PagoxDia = double.Parse(Console.ReadLine());
+
+            Console.Write("Cuántos días trabajastes?: ");
+            Dias = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El dinero obtenido por los días trabajados es:" + CalcularTotal(PagoxDia, Dias));
+
+            Console.ReadKey();
         }
 
-        static void numeros()
+        //Método para obtener el total de dinero ganado por trabajo
+        static double CalcularTotal(double P1, int P2)
         {
-
+            double Total;
+            Total = (P1 * P2);
+            return Total;
         }
     }
 }
