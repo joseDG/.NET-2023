fc5f0680a9c2e607b6a4bb74eb89193dfe81999a
JoseDG <josethp1@gmail.com>
2023-12-13 09:32:54 -0500

    add ciclica while

---
+++ CSharp_NivelBasico/12-EstructuraCiclicaWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_EstructuraCiclicaWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Correlativo;
+            Correlativo = 0;
+            Console.WriteLine("La tabla #5");
+
+            while(Correlativo < 12)
+            {
+                Console.WriteLine("5 x " + Correlativo + " = " + (5 * Correlativo));
+                Correlativo++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
