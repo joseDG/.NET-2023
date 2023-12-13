c0cda2a62df89ca91f6929d63fd318495415a2de
JoseDG <josethp1@gmail.com>
2023-12-13 09:48:54 -0500

    matrices

---
+++ CSharp_NivelBasico/14-Vectores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nombres = { "Victor", "Carlos", "Ana", "Luis", "Jorge" };
+            
+            for(int x=0; x<Nombres.Length; x++)
+            {
+                Console.WriteLine("Contenido del Vector Nombres en el indice" + x + "es:" + Nombres[x]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
