29abeb3dcb10695a734a76f1efdfaed50a08e7f1
JoseDG <josethp1@gmail.com>
2023-12-13 09:59:21 -0500

    add metodos 3

---
+++ CSharp_NivelBasico/18-MetodosEjemplo3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_MetodosEjemplo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void numeros()
+        {
+
+        }
+    }
+}
