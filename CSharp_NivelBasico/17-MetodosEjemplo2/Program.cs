bf3f658bafcd5a5bf177a166e5a8a61b060d8c46
JoseDG <josethp1@gmail.com>
2023-12-13 10:51:18 -0500

    update metodos 2

--- CSharp_NivelBasico/17-MetodosEjemplo2/Program.cs
+++ CSharp_NivelBasico/17-MetodosEjemplo2/Program.cs
@@ -10,11 +10,47 @@ namespace _17_MetodosEjemplo2
     {
         static void Main(string[] args)
         {
-            
+            string OperacionMatematica;
+            int Noperacion;
+
+            Console.Write("Quieres realizar la tabla de Sumar o Multiplicar?: ");
+            OperacionMatematica = Console.ReadLine();
+            Console.Write("Que Número de tabla desea que se muestre: ");
+            Noperacion = int.Parse(Console.ReadLine());
+
+            if (OperacionMatematica == "SUMAR")
+            {
+                Sumar(Noperacion);
+            }
+            else if (OperacionMatematica == "MULTIPLICAR")
+            {
+                Multiplicar(Noperacion);
+            }
+            else
+            {
+                Console.WriteLine("Operación matemática no válida");
+            }
+
+            Console.ReadKey();
+
         }
+
+        //método de la tabla de Sumar
         static void Sumar(int Numero)
         {
+            for (int a = 1; a <= 12; a++)
+            {
+                Console.WriteLine(Numero + " + " + a + " = " + (Numero + a));
+            }
+        }
 
+        //método de la tabla de Multiplicar
+        static void Multiplicar(int Numero)
+        {
+            for (int a = 1; a <= 12; a++)
+            {
+                Console.WriteLine(Numero + " X " + a + " = " + (Numero * a));
+            }
         }
     }
 }
