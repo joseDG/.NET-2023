fa033a319e328ea24cde3d4012cf9d317fbbdee2
JoseDG <josethp1@gmail.com>
2023-12-13 09:39:24 -0500

    add For

---
+++ CSharp_NivelBasico/13-InstruccionFor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_InstruccionFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 0;
+
+            Console.Write("Ingrese un nro para definir la Tabla de Multiplicar: ");
+            i = int.Parse(Console.ReadLine());
+
+            for(int a = 0; a <= 12; a++)
+            {
+                Console.WriteLine(i + " X " + a + " = " + (i * a));
+            }
+            Console.ReadKey();
+        }
+    }
+}
