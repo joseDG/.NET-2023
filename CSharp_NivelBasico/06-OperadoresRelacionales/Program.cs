3c82c16e6c4cd53a79ad9c2b78fd425e7b084a70
JoseDG <josethp1@gmail.com>
2023-12-13 08:37:17 -0500

    add operadores relacionaes

---
+++ CSharp_NivelBasico/06-OperadoresRelacionales/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_OperadoresRelacionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2;
+            bool Operaciones;
+
+            Num1 = 10;
+            Num2 = 12;
+            Operaciones = (Num1 != Num2);
+
+            Console.WriteLine("Num1 es distinto que Num2? "+ Operaciones);
+            Console.ReadKey();
+        }
+    }
+}
