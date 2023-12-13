64b9b8d6168120f5f7410e5fe2822e33d6365fe8
JoseDG <josethp1@gmail.com>
2023-12-13 10:48:27 -0500

    update

--- CSharp_NivelBasico/08-ConversionTiposDatos/Program.cs
+++ CSharp_NivelBasico/08-ConversionTiposDatos/Program.cs
@@ -10,7 +10,27 @@ namespace _08_ConversionTiposDatos
     {
         static void Main(string[] args)
         {
-            int numero = 0;
+            String Nombre;
+            int Edad;
+            bool Casado;
+            double Sueldo;
+
+            Console.Write("Ingrese su Nombre: ");
+            Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su edad: ");
+            //Edad = int.Parse(Console.ReadLine());
+            Edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Eres Casado?: ");
+            //Casado = bool.Parse(Console.ReadLine());
+            Casado = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("Tu Sueldo: ");
+            //Sueldo = double.Parse(Console.ReadLine());
+            Sueldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.ReadKey();
         }
     }
 }
