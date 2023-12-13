6c3ecb94cda548794da2717db5d2bc00daa5d155
JoseDG <josethp1@gmail.com>
2023-12-13 10:59:05 -0500

    update estructuras de selecion

--- CSharp_NivelBasico/10-EstructurasSelectivas/Program.cs
+++ CSharp_NivelBasico/10-EstructurasSelectivas/Program.cs
@@ -11,7 +11,41 @@ namespace _10_EstructurasSelectivas
     {
         static void Main(string[] args)
         {
-            string dia = "Lunes";
+            int Dia;
+
+            Console.Write("Ingrese el día en número: ");
+            Dia = int.Parse(Console.ReadLine());
+
+            switch (Dia)
+            {
+                case 1:
+                    Console.WriteLine("ES LUNES");
+                    break;
+                case 2:
+                    Console.WriteLine("ES MARTES");
+                    break;
+                case 3:
+                    Console.WriteLine("ES MIÉRCOLES");
+                    break;
+                case 4:
+                    Console.WriteLine("ES JUEVES");
+                    break;
+                case 5:
+                    Console.WriteLine("ES VIERNES");
+                    break;
+                case 6:
+                    Console.WriteLine("ES SÁBADO");
+                    break;
+                case 7:
+                    Console.WriteLine("ES DOMINGO");
+                    break;
+
+                default:
+                    Console.WriteLine("EL NRO DE DÍA INGRESADO NO ES VÁLIDO");
+                    break;
+            }
+            Console.ReadKey();
+
         }
     }
 }
