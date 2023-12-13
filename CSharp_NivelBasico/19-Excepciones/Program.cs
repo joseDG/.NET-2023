6f29571fafc3a399bf79c93e048261528f203ef3
JoseDG <josethp1@gmail.com>
2023-12-13 10:55:42 -0500

    update excepiones

---
+++ CSharp_NivelBasico/19-Excepciones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Edad;
+
+            try
+            {
+                Console.Write("Ingresa tu Edad: ");
+                Edad = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
