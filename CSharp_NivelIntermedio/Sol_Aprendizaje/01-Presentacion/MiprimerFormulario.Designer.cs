97310327480aa7a198c07192072641e1d96ee84e
JoseDG <josethp1@gmail.com>
2023-12-14 11:48:18 -0500

    add 01 - formulario

---
+++ CSharp_NivelIntermedio/Sol_Aprendizaje/01-Presentacion/MiprimerFormulario.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace _01_Presentacion
+{
+    partial class Formulario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulario));
+            this.btn_nuevo = new System.Windows.Forms.Button();
+            this.btn_actualizar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_reporte = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_Codigo = new System.Windows.Forms.TextBox();
+            this.textDescripcion = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_nuevo
+            // 
+            this.btn_nuevo.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_nuevo.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nuevo.Image = ((System.Drawing.Image)(resources.GetObject("btn_nuevo.Image")));
+            this.btn_nuevo.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_nuevo.Location = new System.Drawing.Point(45, 503);
+            this.btn_nuevo.Name = "btn_nuevo";
+            this.btn_nuevo.Size = new System.Drawing.Size(163, 102);
+            this.btn_nuevo.TabIndex = 3;
+            this.btn_nuevo.Text = "Nuevo";
+            this.btn_nuevo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_nuevo.UseVisualStyleBackColor = false;
+            this.btn_nuevo.Click += new System.EventHandler(this.btn_nuevo_Click);
+            // 
+            // btn_actualizar
+            // 
+            this.btn_actualizar.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_actualizar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_actualizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_actualizar.Image")));
+            this.btn_actualizar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_actualizar.Location = new System.Drawing.Point(231, 503);
+            this.btn_actualizar.Name = "btn_actualizar";
+            this.btn_actualizar.Size = new System.Drawing.Size(157, 102);
+            this.btn_actualizar.TabIndex = 4;
+            this.btn_actualizar.Text = "Actualizar";
+            this.btn_actualizar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_actualizar.UseVisualStyleBackColor = false;
+            this.btn_actualizar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_eliminar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_eliminar.Image = ((System.Drawing.Image)(resources.GetObject("btn_eliminar.Image")));
+            this.btn_eliminar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_eliminar.Location = new System.Drawing.Point(408, 503);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(157, 102);
+            this.btn_eliminar.TabIndex = 5;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_eliminar.UseVisualStyleBackColor = false;
+            // 
+            // btn_reporte
+            // 
+            this.btn_reporte.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_reporte.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_reporte.Image = ((System.Drawing.Image)(resources.GetObject("btn_reporte.Image")));
+            this.btn_reporte.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_reporte.Location = new System.Drawing.Point(593, 503);
+            this.btn_reporte.Name = "btn_reporte";
+            this.btn_reporte.Size = new System.Drawing.Size(173, 102);
+            this.btn_reporte.TabIndex = 6;
+            this.btn_reporte.Text = "Reporte";
+            this.btn_reporte.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_reporte.UseVisualStyleBackColor = false;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackColor = System.Drawing.SystemColors.Window;
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Image = ((System.Drawing.Image)(resources.GetObject("btn_salir.Image")));
+            this.btn_salir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btn_salir.Location = new System.Drawing.Point(792, 503);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(155, 102);
+            this.btn_salir.TabIndex = 7;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_salir.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Codigo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // text_Codigo
+            // 
+            this.text_Codigo.Enabled = false;
+            this.text_Codigo.Location = new System.Drawing.Point(231, 117);
+            this.text_Codigo.MaxLength = 3;
+            this.text_Codigo.Name = "text_Codigo";
+            this.text_Codigo.Size = new System.Drawing.Size(149, 26);
+            this.text_Codigo.TabIndex = 1;
+            // 
+            // textDescripcion
+            // 
+            this.textDescripcion.Enabled = false;
+            this.textDescripcion.Location = new System.Drawing.Point(231, 172);
+            this.textDescripcion.MaxLength = 30;
+            this.textDescripcion.Name = "textDescripcion";
+            this.textDescripcion.Size = new System.Drawing.Size(458, 26);
+            this.textDescripcion.TabIndex = 2;
+            this.textDescripcion.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Descripcion:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Formulario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1210, 645);
+            this.Controls.Add(this.textDescripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.text_Codigo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_reporte);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_actualizar);
+            this.Controls.Add(this.btn_nuevo);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Formulario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formulario Mantenimiento";
+            this.Load += new System.EventHandler(this.MiprimerFormulario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_nuevo;
+        private System.Windows.Forms.Button btn_actualizar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_reporte;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_Codigo;
+        private System.Windows.Forms.TextBox textDescripcion;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ CSharp_NivelIntermedio/Sol_Aprendizaje/01-Presentacion/MiprimerFormulario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_Presentacion
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void MiprimerFormulario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_nuevo_Click(object sender, EventArgs e)
+        {   
+            //limpiar cajas de texto
+            text_Codigo.Text = "";
+            textDescripcion.Text = "";
+
+            //activar cajas de texto
+            text_Codigo.Enabled = true;
+            textDescripcion.Enabled = true;
+
+            //inicia el cursor al inicio
+            //text_Codigo.Select();
+            text_Codigo.Focus();
+        }
+    }
+}
