9a2cb01467938aeb52933a516576b59c9e77467c
JoseDG <josethp1@gmail.com>
2023-12-26 10:56:02 -0500

    add abstract

---
+++ PildorasInformaticasCourse/52-ClasesAbstractas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_ClasesAbstractas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Acutangulo acutangulo = new Acutangulo();
+            Escaleno escaleno = new Escaleno();
+
+            acutangulo.Area();
+            escaleno.Area();
+
+            escaleno.CalcularAreaTrianguloHipotenusa(6, 8);
+            acutangulo.CalcularAreaTrianguloHipotenusa(2, 5);
+        }
+    }
+}
---
+++ PildorasInformaticasCourse/52-ClasesAbstractas/Triaangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_ClasesAbstractas
+{
+    public abstract class Triaangulo
+    {
+        public abstract decimal Perimetro();
+        public abstract decimal Area();
+
+        public double CalcularAreaTrianguloHipotenusa(int lado, int hipotenusa)
+        {
+            double ladob = Math.Sqrt(Math.Pow(hipotenusa, 2) - Math.Pow(lado, 2));
+            return lado - ladob / 2;
+        }
+    }
+
+    public class Escaleno : Triaangulo
+    {
+        public override decimal Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal Perimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Acutangulo : Triaangulo
+    {
+        public override decimal Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override decimal Perimetro()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
