6a85ed0a4c05b0a941dde8ecbb3fc6e34fa56f63
JoseDG <josethp1@gmail.com>
2024-01-10 09:25:08 -0500

    update seccion

---
+++ CursoCompletoC#/CursoCompletoC#/01-PedirDatos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_PedirDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Preclentar el horno");
+            Console.WriteLine("2. Mezclar harina y mantequilla");
+            Console.WriteLine("3. Agregar azucar y 1 huevo");
+            Console.WriteLine("4. Amasar la mezcla");
+            Console.WriteLine("5. Darles forma y colocar en charola");
+            Console.WriteLine("6. Hornear durante 10 minutos");
+        }
+    }
+}
