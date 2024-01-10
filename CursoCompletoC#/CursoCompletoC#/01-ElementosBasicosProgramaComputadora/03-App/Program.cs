8ae0943aa208a61c1ac87927fbd33bf3e9c0e374
JoseDG <josethp1@gmail.com>
2024-01-10 18:30:38 -0500

    update

--- CursoCompletoC#/CursoCompletoC#/01-ElementosBasicosProgramaComputadora/03-App/Program.cs
+++ CursoCompletoC#/CursoCompletoC#/01-ElementosBasicosProgramaComputadora/03-App/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _03_App
@@ -10,6 +11,19 @@ namespace _03_App
     {
         static void Main(string[] args)
         {
+            //string saludo = "Hola";
+            //char caracter = 'a';
+
+            //double i = 8.9;
+            //int numero1 = 78;
+
+            //Console.WriteLine(i);
+            //Console.WriteLine(numero1);
+
+            double precioCamisa = 399;
+            string colorCamisa = "Azul";
+
+            Console.WriteLine("El precio de la camisa es: {0} y su color: {1}", precioCamisa, colorCamisa);
         }
     }
 }
