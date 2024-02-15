818f9566d1603858e957ce4d6280c42b13bd58e2
JoseDG <josethp1@gmail.com>
2024-02-14 22:14:56 -0500

    start estructuras

---
+++ CursoCompletoC#/CursoCompletoC#/09-Estructuras-Enumeraciones/02-App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciamos a la estructura
+            Alumno alumno1 = new Alumno();
+
+            //asignaaanodel el valor a un campo
+            alumno1.Nombre = "Luis";
+            //Mostrar el valor del campo "Nombre"
+            Console.WriteLine($"El nombre del alumno es: {alumno1.Nombre}");
+
+        }
+    }
+
+    struct Alumno
+    {
+        //campos
+        string nombre;
+        string apellido;
+        string calificacion;
+        string alergias;
+        string numeroTelefono;
+
+        //creamos una propiedaad para nombre
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+}
---
+++ CursoCompletoC#/CursoCompletoC#/09-Estructuras-Enumeraciones/enumeracion/01-App/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipos de referencia integrado
+            //con palabra clave
+            string cadena1;
+            //con tipo .NET
+            System.String cadena2;
+            //con tipo .NET simplificado
+            String cadena3;
+
+            //TIPOS DE VALOR INTEGRADO
+            //Con palabra claave
+            int enetero1;
+            //con tipoi .NET
+            System.Int32 entero2;
+            //con tipo .NET simplificado
+            Int32 entero3;
+        }
+    }
+}
